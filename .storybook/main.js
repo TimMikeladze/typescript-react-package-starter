b2a6859bd3fd6574f7cb1d85cc926d4354af11ef
TimMikeladze <tim.mikeladze@gmail.com>
2022-05-31 00:22:06 +0200

    feat: 🎸 package updates and bug fixes

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,5 +8,12 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react"
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5",
+  },
+  "typescript": {
+    "check": false,
+    "reactDocgen": false
+  },
 }
