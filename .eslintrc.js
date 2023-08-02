4cee9dd2475bf8a347c3ef77f0c310e0c466cb4e
TimMikeladze <tim.mikeladze@gmail.com>
2023-08-02 16:08:10 -0700

    feat: added eslint-plugin-sort-class-members

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
     'unused-imports',
     'prettier',
     'prefer-arrow',
+    'sort-class-members',
   ],
   rules: {
     '@typescript-eslint/no-unused-vars': 'error',
@@ -51,6 +52,21 @@ module.exports = {
         disallowPrototype: true,
         singleReturnOnly: true,
         classPropertiesAllowed: false,
+      },
+    ],
+    'sort-class-members/sort-class-members': [
+      'error',
+      {
+        order: [
+          '[static-properties]',
+          '[properties]',
+          '[conventional-private-properties]',
+          'constructor',
+          '[static-methods]',
+          '[methods]',
+          '[conventional-private-methods]',
+        ],
+        accessorPairPositioning: 'getThenSet',
       },
     ],
   },
