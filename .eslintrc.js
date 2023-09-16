2eeb750216119e934a9801fc70ecce4960b84fce
TimMikeladze <tim.mikeladze@gmail.com>
2023-09-16 11:00:11 -0700

    feat: add react-in-jsx-scope rule to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,6 +43,7 @@ module.exports = {
     'react/jsx-filename-extension': 'off',
     'react/jsx-props-no-spreading': 'off',
     'react/require-default-props': 'off',
+    'react/react-in-jsx-scope': 'off',
     'typescript-sort-keys/interface': 'error',
     'typescript-sort-keys/string-enum': 'error',
     'unused-imports/no-unused-imports': 'error',
