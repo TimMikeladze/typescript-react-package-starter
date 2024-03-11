16ad494b1030a429a0996b3ed3b33b76072268c1
Tim Mikeladze <tim.mikeladze@gmail.com>
2024-03-12 00:58:11 +0100

    feat: update storybook to v8

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -5,6 +5,7 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
+    '@storybook/addon-webpack5-compiler-swc',
   ],
   framework: {
     name: '@storybook/react-webpack5',
